69890ec96c58c14cb0b70d1fb0de92c0a8d6820f
Azat Valiev <azat@valievventures.com>
2024-03-07 08:51:26 -0600

    add typecheck

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     dir: 'src',
-    useAtomics: true,
     reporters: ['html', 'verbose'],
     watch: false,
     include: ['**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
